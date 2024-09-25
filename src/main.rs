96297948a3e6ef44f4c033004055624e73e552f8
karankharode <karankharode.kk@gmail.com>
2024-09-25 14:10:30 +0530

    Chapter 3: Telemetry - add logging

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ use env_logger::Env;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-
+    
     let configuration = get_configuration().expect("Failed to get configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind express");
