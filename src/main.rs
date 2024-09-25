d3e9ec0025fc5d27454ceebd0b8528f231d843bf
karankharode <karankharode.kk@gmail.com>
2024-09-25 16:05:35 +0530

    add: create telemetry module and use in main file

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-use email_newsletter::startup::run;
-use sqlx::{Connection, PgPool};
-use std::net::TcpListener;
 use email_newsletter::configuration::get_configuration;
-use env_logger::Env;
+use email_newsletter::startup::run;
+use sqlx::PgPool;
+use std::net::TcpListener;
+use email_newsletter::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    
+    let subscriber = get_subscriber("email_newsletter".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to get configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind express");
 
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
-    .await.expect("Failed to connect to database");
+        .await
+        .expect("Failed to connect to database");
     run(listener, connection_pool)?.await
 }
 
-/* 
+/*
  * Did you know you can embed your migrations in your application binary?
 On startup, after creating your database connection or pool, add:
 
